0508cf861d3ae689680798acf88d32e32c60b170
vipyctmall <vipyctmall@gmail.com>
2026-07-28 18:57:17 +0800

    Create counter-config.js

---
+++ assets/js/counter-config.js
@@ -0,0 +1,12 @@
+/**
+ * AKG Global / vipyctmall.com Cloudflare D1 訪客統計設定
+ *
+ * 正式網域：https://vipyctmall.com/
+ * Worker：https://vipyctmall-visit.vipyctmall.workers.dev
+ * D1 siteKey：vipyctmall-mall
+ */
+window.AKG_COUNTER = Object.freeze({
+  apiBase: "https://vipyctmall-visit.vipyctmall.workers.dev",
+  siteKey: "vipyctmall-mall",
+  canonicalSite: "https://vipyctmall.com/"
+});
