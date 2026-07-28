6daabde5497604200f7e4d87058cd02fe1414fea
vipyctmall <vipyctmall@gmail.com>
2026-07-28 21:25:58 +0800

    Update counter-config.js

--- assets/js/counter-config.js
+++ assets/js/counter-config.js
@@ -1,10 +1,4 @@
-/**
- * AKG Global / vipyctmall.com Cloudflare D1 訪客統計設定
- *
- * 正式網域：https://vipyctmall.com/
- * Worker：https://vipyctmall-visit.vipyctmall.workers.dev
- * D1 siteKey：vipyctmall-mall
- */
+/** AKG Global visitor counter */
 window.AKG_COUNTER = Object.freeze({
   apiBase: "https://vipyctmall-visit.vipyctmall.workers.dev",
   siteKey: "vipyctmall-mall",
